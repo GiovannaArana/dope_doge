8f80923371c6457e56f5d24e802fdda226b02b68
4n7hem <48725919+4n7hem@users.noreply.github.com>
2021-01-31 13:42:23 -0300

    A mecanica mais importante foi implementada

--- New Unity Project/Assets/Script/Moves/Bonk.cs
+++ New Unity Project/Assets/Script/Moves/Bonk.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Bonk : MonoBehaviour, IMoveBehaviour {
     
     private GameObject player;
+	private funcoesDeMovimento funcs;
 
 	public Bonk(GameObject player){
 
 		this.player = player;
+		funcs = player.GetComponent<funcoesDeMovimento>();
 	}
 
 	public IMoveBehaviour GetMove(GameObject player){
@@ -27,8 +29,14 @@ public class Bonk : MonoBehaviour, IMoveBehaviour {
     	Vector3 pos = player.transform.position + direction;
         Collider2D col2d = Physics2D.OverlapBox(pos, new Vector2(1f, 1f), 1f, 1);
 
-        if(col2d != null && col2d.gameObject.tag == "Box")
+        if(col2d != null && col2d.gameObject.tag == "Box"){
+			funcs.Bonk(player);
         	col2d.gameObject.GetComponent<BoxBehaviour>().Explode();
+		}
+		if(col2d != null && col2d.gameObject.tag == "Trash"){
+			funcs.Bonk(player);
+			col2d.gameObject.GetComponent<Trash>().Explode();
+		}
     }
 
     public void PseudoUpdate(){}
--- New Unity Project/Assets/Script/funcoesDeMovimento.cs
+++ New Unity Project/Assets/Script/funcoesDeMovimento.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class funcoesDeMovimento : MonoBehaviour
 {
-    public void AndarComecou(){
-        GameObject dog = GameObject.FindWithTag("Dog");
-        Animator anim = dog.GetComponent<Animator>();
+    public void AndarComecou(GameObject player){  
+        Animator anim = player.GetComponent<Animator>();      
         anim.SetTrigger("Andar");
+    }
+
+    public void Bonk(GameObject player){
+        Animator anim = player.GetComponent<Animator>();      
+        anim.SetTrigger("Bonk");
     }
 }
